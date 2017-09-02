fc94b6eb328b44f1175321bf32ccf4bc883aebb1
svkreml <svkreml@gmail.com>
2017-09-02 16:54:52 +0300

    Base

---
+++ clockWifiWemos.ino
@@ -0,0 +1,207 @@
+// Demo of MAX7219_Dot_Matrix library - sideways scrolling
+// Author: Nick Gammon
+// Date: 2 October 2015
+
+// Scrolls a pixel at a time.
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+#include <bitBangedSPI.h>
+#include <MAX7219_Dot_Matrix.h>
+
+const byte chips = 5;
+
+const char *ssid = "-";
+const char *pass = "-";
+
+
+static const unsigned int localPort = 2390;      // local port to listen for UDP packets
+
+
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char *ntpServerName = "time.nist.gov";
+
+static const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+
+unsigned long getTimeEpoch() {
+    //get a random server from the pool
+    WiFi.hostByName(ntpServerName, timeServerIP);
+
+    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+    // wait to see if a reply is available
+    delay(1000);
+
+    int cb = udp.parsePacket();
+    if (!cb) {
+        Serial.println("no packet yet");
+    } else {
+        Serial.print("packet received, length=");
+        Serial.println(cb);
+        // We've received a packet, read the data from it
+        udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+        //the timestamp starts at byte 40 of the received packet and is four bytes,
+        // or two words, long. First, esxtract the two words:
+
+        unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+        unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+        // combine the four bytes (two words) into a long integer
+        // this is NTP time (seconds since Jan 1 1900):
+        unsigned long secsSince1900 = highWord << 16 | lowWord;
+        Serial.print("Seconds since Jan 1 1900 = ");
+        Serial.println(secsSince1900);
+
+        // now convert NTP time into everyday time:
+        Serial.print("Unix time = ");
+        // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+        const unsigned long seventyYears = 2208988800UL;
+        // subtract seventy years:
+        unsigned long epoch = secsSince1900 - seventyYears;
+        // print Unix time:
+        
+        Serial.println(epoch);
+
+  // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+
+
+
+        epoch+=3600*3;
+        return epoch;
+    }
+    return 0;
+};
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress &address) {
+    Serial.println("sending NTP packet...");
+    // set all bytes in the buffer to 0
+    memset(packetBuffer, 0, NTP_PACKET_SIZE);
+    // Initialize values needed to form NTP request
+    // (see URL above for details on the packets)
+    packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+    packetBuffer[1] = 0;     // Stratum, or type of clock
+    packetBuffer[2] = 6;     // Polling Interval
+    packetBuffer[3] = 0xEC;  // Peer Clock Precision
+    // 8 bytes of zero for Root Delay & Root Dispersion
+    packetBuffer[12] = 49;
+    packetBuffer[13] = 0x4E;
+    packetBuffer[14] = 49;
+    packetBuffer[15] = 52;
+
+    // all NTP fields have been given values, now
+    // you can send a packet requesting a timestamp:
+    udp.beginPacket(address, 123); //NTP requests are to port 123
+    udp.write(packetBuffer, NTP_PACKET_SIZE);
+    udp.endPacket();
+};
+// 12 chips (display modules), hardware SPI with load on D10
+MAX7219_Dot_Matrix display(chips, 2);  // Chips / LOAD 
+unsigned long epoch;
+char message[] = "00000";
+
+void setup() {
+    display.begin();
+    display.setIntensity(0);
+
+
+    
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println();
+
+    // We start by connecting to a WiFi network
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.println("Starting UDP");
+    udp.begin(localPort);
+    Serial.print("Local port: ");
+    Serial.println(udp.localPort());
+}  // end of setup
+
+
+
+void updateDisplay(char message[], unsigned long epoch) {
+  
+    int hour = (epoch % 86400L) / 3600;
+    int minute = (epoch % 3600) / 60;
+    int sec = epoch % 60;
+    
+    if (hour < 10)
+        message[0] = '0';
+    else message[0] = hour / 10 + '0';
+         message[1] = hour % 10 + '0';
+
+         
+    if (sec % 2 == 1)
+        message[2] = ':';
+    else
+        message[2] = '.';
+
+        
+    if (minute < 10)
+        message[3] = '0';
+    else message[3] = minute / 10+'0';
+    message[4] = minute % 10+'0';
+
+    
+    Serial.println("-------------------------------------------");
+     Serial.println(epoch);
+     Serial.print(hour);
+      Serial.print(" : ");
+       Serial.print(minute);
+        Serial.print(" : ");
+         Serial.println(sec);
+     Serial.println(message);
+    display.sendSmooth(message, 0);
+}  // end of updateDisplay
+int timeToUpdate=0;
+//unsigned long t1000 = 1000;
+void loop() {
+  if(millis()%1000==0){
+  if(timeToUpdate>3600||epoch<9000000){
+    timeToUpdate=0;
+    epoch = getTimeEpoch();
+  }
+    else{
+      epoch++;
+    }
+    updateDisplay(message, epoch);
+    timeToUpdate++;
+  }
+}  // end of loop
+
+
+
+
